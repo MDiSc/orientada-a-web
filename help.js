b7672b5532822a45063af9a1b1418da0ee0af620
Sam7AR <samazuaje2k4w@gmail.com>
2024-12-30 01:16:05 -0400

    some css changes added, validated that only one player of each can be created, pretty rough pre-version of login system

--- help.js
+++ help.js
@@ -1,3 +1,63 @@
+let loggedInPlayers = [];
+let playerPasswords = [];
+let supportedPLayers = ['player1','player2','player3','player4'];
+
+let playersList = [];
+
+console.log(typeof playersList);
+
+class Player{
+    constructor(username,password){
+        this.username = username;
+        this.password = password;
+    }
+}
+
+function savePlayer(player){
+    let playerToSave = JSON.stringify(player);
+    let parsedPlayer = JSON.parse(playerToSave);
+    console.log(`Player to save ${playerToSave}`);
+    console.log(parsedPlayer);
+}
+function loadPlayers(){}
+
+function printPlayers(players) {
+    players.forEach(player => {
+        console.log(`Username: ${player.username}, Password: ${player.password}`);
+    });
+}
+function validateLogIn(user,password){
+    
+    if(playersList.some(player => (player.username === user))){
+        if(playersList.some(player => ((player.username === user)&&(player.password===password)))){
+            alert('Log In Succesful!');
+            return true;
+        }else{
+            alert('Invalid password');
+            return false;
+        }
+    }    
+    else{
+        alert('User not registered');
+        return false;
+    }    
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('log-in');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const loginBox = document.querySelector('a[data-text=iniciar-sesion]');
+        const username = document.getElementById('username-log-in').value;
+        const password = document.getElementById('password-log-in').value;
+    
+        if(validateLogIn(username,password)){
+            form.style = 'display: none';
+            loginBox.innerHTML = `Hello, ${username}`;
+        } 
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registro-usuario');
     form.addEventListener('submit', function(event) {
@@ -6,8 +66,22 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = document.getElementById('password').value;
         const gameMode = document.querySelector('input[name="game-mode"]:checked').value;
         const player = document.querySelector('input[name="player"]:checked').value;
-        console.log(username, password, gameMode, player);
-        createTable(`battleship-board-${player}`, username, gameMode);
+        const currentPlayer = document.getElementById(player);
+        if(currentPlayer.dataset.exists==='false'){
+            if(playersList.some(player => player.username === username)){
+                alert('Username already in use! Try another one')
+            }else{
+                console.log(username, password, gameMode, player);
+                createTable(`battleship-board-${player}`, username, gameMode);
+                currentPlayer.dataset.exists='true';
+                playersList.push(new Player(username,password));
+                savePlayer(playersList.find(player => player.username===username));
+                console.log(typeof playersList);
+                printPlayers(playersList);
+            }
+        }else{
+            alert(`${player} already created`);
+        }
     });
 });
 function createTable(name, userName, gameMode){
@@ -72,20 +146,22 @@ function placeShip(shipType, startCoordinates, direction){
 }
 
 
-const multiPlayerRadio = document.getElementById('multi-player');
-const singlePLayerRadio = document.getElementById('single-player');
-const multiplayerDiv = document.getElementById('multiplayer-div');
-const foreverAloneDiv = document.getElementById('forever-alone-div');
+
 
 
 function togglePLayerOptions() {
+    const multiPlayerRadio = document.getElementById('multi-player');
+    const singlePLayerRadio = document.getElementById('single-player');
+    const multiplayerDiv = document.getElementById('multiplayer-div');
+    const foreverAloneDiv = document.getElementById('forever-alone-div');
+
     if (multiPlayerRadio.checked) {
-        multiplayerDiv.style.display = 'block'; 
-        foreverAloneDiv.style = 'none';
+        multiplayerDiv.style = 'display: block'; 
+        foreverAloneDiv.style = 'display: block';
     } 
     if(singlePLayerRadio.checked){
-        foreverAloneDiv.style = 'block';
-        multiplayerDiv.style.display = 'none';
+        foreverAloneDiv.style = 'display: block';
+        multiplayerDiv.style = 'display: none';
     }
     /*else {
         multiplayerDiv.style.display = 'none';
@@ -98,5 +174,5 @@ document.querySelectorAll('input[name="game-mode"]').forEach(radio => {
     radio.addEventListener('change', togglePLayerOptions);
 });
 
-
 togglePLayerOptions();
+
