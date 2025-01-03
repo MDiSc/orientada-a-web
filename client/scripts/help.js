d2297b14cff09282479dac9cb49a117989fed188
Maurizio Di Scipio <mauriziodiscipio@gmail.com>
2025-01-03 15:12:57 -0400

    added base ws logic

--- client/scripts/help.js
+++ client/scripts/help.js
@@ -1,115 +1,21 @@
-let loggedInPlayers = [];
-let playerPasswords = [];
-let supportedPLayers = ['player1','player2','player3','player4'];
-
-let playersList = [];
-
-console.log(typeof playersList);
-
-class Player{
-    constructor(username,password){
-        this.username = username;
-        this.password = password;
-    }
-}
-
-/*function savePlayer(player) {
-    playersList.push(player); // Agrega el nuevo jugador a la lista
-     // Convertir la lista de jugadores a formato JSON
-     const jsonData = JSON.stringify(playersList, null, 2); // El segundo parámetro es para formatear el JSON
-
-     // Guardar el archivo
-     fs.writeFile('players.json', jsonData, (err) => {
-         if (err) {
-             console.error('Error al guardar el archivo:', err);
-         } else {
-             console.log('Archivo players.json guardado exitosamente.');
-         }
-     });
-}*/
-
-function savePlayer2(player){
-    let playerToSave = JSON.stringify(player);
-    let parsedPlayer = JSON.parse(playerToSave);
-    console.log(`Player to save ${playerToSave}`);
-    console.log(parsedPlayer);
-}
-
-function savePlayersToJSON() {
-    const jsonPlayers = JSON.stringify(playersList); // Convertir a JSON
-    localStorage.setItem('players.json', jsonPlayers); // Guardar en localStorage
-}
-
-function loadPlayers(){}
-
-function printPlayers(players) {
-    players.forEach(player => {
-        console.log(`Username: ${player.username}, Password: ${player.password}`);
-    });
-}
-function validateLogIn(user,password){
-    
-    if(playersList.some(player => (player.username === user))){
-        if(playersList.some(player => ((player.username === user)&&(player.password===password)))){
-            alert('Log In Succesful!');
-            return true;
-        }else{
-            alert('Invalid password');
-            return false;
-        }
-    }    
-    else{
-        alert('User not registered');
-        return false;
-    }    
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('log-in');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const loginBox = document.querySelector('a[data-text=iniciar-sesion]');
-        const username = document.getElementById('username-log-in').value;
-        const password = document.getElementById('password-log-in').value;
-    
-        if(validateLogIn(username,password)){
-            form.style = 'display: none';
-            loginBox.innerHTML = `Hello, ${username}`;
-        } 
-    });
-});
-
-
 let shipCount = 0;
 
 const socket = new WebSocket('ws://localhost:8080');
 socket.addEventListener('open', function (event) {
     console.log('Connected to WebSocket server');
 });
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-    const message = JSON.parse(event.data);
-    if (message.type == 'updateBoard') {
-        updateBoard(message.boardId, message.boardData);
-    }
-});
 
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('registro-usuario');
+    const form = document.getElementById('register');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        const gameMode = document.querySelector('input[name="game-mode"]:checked').value;
-        const player = document.querySelector('input[name="player"]:checked').value;
-        console.log(username, password, gameMode, player);
         const deck = document.getElementById('deck');
         const placedShips = document.getElementById('placed-ships');
         deck.style.display = 'block';
         placedShips.style.display = 'block';
         createTable(`battleship-board-1`, username);
         form.style.display = 'none';
-
     });
 });
 function createTable(name, userName){
